07016ae5e10b55377d7faeacfb89c0dbd932bf90
Vykstorm <victorruizgomezdev@gmail.com>
2017-09-27 19:36:45 +0200

    Se han añadido las metafunciones __hash__ y __user__

--- model/user.py
+++ model/user.py
@@ -1,6 +1,7 @@
 
 
 
+
 class TwitterUser(dict):
     '''
     Esta clase representa a un usuario. Provee información relativa a su
@@ -21,7 +22,34 @@ class TwitterUser(dict):
     def get_name(self):
         return self['name']
 
+    @staticmethod
+    def search_by_id(id):
+        '''
+        Devuelve el usuario cuya ID es la que se especifica como parámetro, o None si no hay
+        nadie que tenga esa ID.
+        :param id:
+        :return:
+        '''
+        return Twitter()._search_user_by_id(id)
+
+    @staticmethod
+    def search_by_name(name):
+        '''
+        Devuelve el usuario cuyo nombre (Screen Name) es el que se indica como parámetro, o None
+        si no hay nadie con ese nombre.
+        :param name:
+        :return:
+        '''
+        return Twitter()._search_user_by_name(name)
+
 
     def __str__(self):
         return self.get_name()
-    
+
+    def __hash__(self):
+        return hash(self.__repr__())
+
+    def __eq__(self, other):
+        return isinstance(other, TwitterUser) and self.get_name() == other.get_name()
+
+from model.twitter_utils import Twitter
